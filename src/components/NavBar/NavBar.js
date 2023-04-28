67e53910ed744d27a2abcbad8befb5a8157e06ae
Tomás Alós <tomasalos@gmail.com>
2023-04-27 22:15:25 -0300

    primeraentregareactjs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,11 +2,11 @@ import CartWidget from "../CartWidget/CartWidget"
 const NavBar = () => {
     return (
         <nav> 
-            <h3 class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"> Sarmiento </h3>
+            <h3 class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"> Kwik-Electro-Mart </h3>
                 <div> 
-                    <button class="btn btn-primary"> Auriculares </button>
-                    <button class="btn btn-primary"> Parlantes </button>
-                    <button class="btn btn-primary"> Cargadores </button>
+                    <button class="btn btn-primary"> Mousepads </button>
+                    <button class="btn btn-primary"> Teclados </button>
+                    <button class="btn btn-primary"> Microfonos </button>
                 </div> 
             <CartWidget/>
         </nav>
